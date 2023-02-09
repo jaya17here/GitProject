f100a1ab0361f8b1f2a5f3e2b101205c42154558
91943 <91943@MSI>
2023-02-10 00:24:34 +0530

    FIRST COMMIT.

---
+++ src/TestGit.java
@@ -0,0 +1,17 @@
+
+public class TestGit {
+     public static void main(String args[])
+     {
+    	 String name = "Mr. Prakash";
+     
+     
+     if(name == "Mr. Prakash")
+     {
+    	System.out.println("Name matched");
+     }
+     else
+     {
+    	 System.out.println("Name mismatched");
+     }
+}
+}
